d475c70ebf8c2692182dc287016b3b19a21388df
istaqom <istaqom@gmail.com>
2024-02-07 01:51:01 +0800

    adding cors for api

--- api/server.js
+++ api/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const db = require("./config/database");
+const cors = require("cors");
 const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
 
 const app = express();
 const port = 8080;
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
